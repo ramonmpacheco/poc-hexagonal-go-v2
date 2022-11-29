12723d1ba70692dc381452e72e0ab617a4f9ec5f
Ramon Pacheco <rmpacheco2@gmail.com>
2022-11-29 08:30:21 -0300

    add: product repo integration test

--- src/test/output/persistence/product/find_product_repository_integration_test.go
+++ src/test/output/persistence/product/find_product_repository_integration_test.go
@@ -26,3 +26,18 @@ func Test_Find_A_Product_By_Id(t *testing.T) {
 
 	assert.EqualValues(t, ids[0], productDomain.ID)
 }
+
+func Test_Return_Error_When_Not_Find_A_Product_By_Id(t *testing.T) {
+	_, err := (&seeds_test.Seeds{DbClient: *db_test.Db}).Product(1)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	missingId := "6385e614198b9a59e233fe1f"
+	findProductRepository := productrepository.NewFindProductRepository(db_test.GetProductCollection())
+	productDomain := &productdomain.Product{ID: missingId}
+	findErr := findProductRepository.ById(productDomain)
+
+	assert.NotNil(t, findErr)
+	assert.EqualValues(t, "no document found for id: "+missingId, findErr.Error())
+}
